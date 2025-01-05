06c4578942e1371a61bda82ad2a5214906a6a206
LenzAlanoca <laalanoca3@umsa.bo>
2025-01-05 00:25:39 -0400

    navre arreglado

--- resources/views/components/bodyHome.blade.php
+++ resources/views/components/bodyHome.blade.php
@@ -1,6 +1,9 @@
-<div class="imge" onclick="alert('¡Texto clickeado!')">
-    <img class="imagenHome" src="https://cdn.pixabay.com/photo/2022/06/21/14/30/background-7275953_1280.jpg"
-        alt="Imagen de fondo">
+<div class="bord" onclick="alert('¡Texto clickeado!')">
+    <!-- Imagen con clases responsivas -->
+    <img class="imagenHome object-cover h-auto"
+        src="https://cdn.pixabay.com/photo/2022/06/21/14/30/background-7275953_1280.jpg" alt="Imagen de fondo">
+
+    <!-- Contenedor centrado -->
     <div class="centerbody">
         <h1>Únete a Inveyt</h1>
     </div>
