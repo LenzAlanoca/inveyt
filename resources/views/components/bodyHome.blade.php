7d1e0c32f437f887f2e95b2288e5634c278f401b
LenzAlanoca <laalanoca3@umsa.bo>
2025-01-04 23:56:53 -0400

    nav arreglado

--- resources/views/components/bodyHome.blade.php
+++ resources/views/components/bodyHome.blade.php
@@ -1,4 +1,4 @@
-<div class="bord" onclick="alert('¡Texto clickeado!')">
+<div class="imge" onclick="alert('¡Texto clickeado!')">
     <img class="imagenHome" src="https://cdn.pixabay.com/photo/2022/06/21/14/30/background-7275953_1280.jpg"
         alt="Imagen de fondo">
     <div class="centerbody">
