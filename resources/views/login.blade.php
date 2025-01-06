41ae34fec59cfe30f28a42674976512d3c4da643
LenzAlanoca <laalanoca3@umsa.bo>
2025-01-05 21:20:33 -0400

    Signines en el Sigin

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,11 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
     <title>Login</title>
 </head>
 
 <body>
-    <h1>Este es el login</h1>
+    <div class="content">
+        <header></header>
+        <main>
+            <h1>Este es el login</h1>
+        </main>
+        <x-footer></x-footer>
+
+    </div>
+
 </body>
 
 </html>
