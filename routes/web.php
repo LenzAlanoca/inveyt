c4efaf51dcfd56db841de1d444f4310d716cc11b
LenzAlanoca <laalanoca3@umsa.bo>
2025-01-05 21:02:01 -0400

    Comenzamos con el Login

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InveytController;
+use App\Http\Controllers\LoginController;
+
 
 
 
 Route::get('/', [InveytController::class, 'index']);
+
+Route::get('/Login', [LoginController::class, 'login']);
 
 Route::get('/perfil', function () {
     return "Bienvenido al perfil ";
