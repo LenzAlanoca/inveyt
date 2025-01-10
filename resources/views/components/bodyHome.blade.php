181db3f6935489a22996d1c49bfc94e92350ad17
JavierMurguiaA <jmurguiaalvarez@gmail.com>
2025-01-09 21:50:35 -0400

    Formulario de login

--- resources/views/components/bodyHome.blade.php
+++ resources/views/components/bodyHome.blade.php
@@ -1,10 +1,24 @@
-<div class="bord" onclick="alert('¡Texto clickeado!')">
-    <!-- Imagen con clases responsivas -->
-    <img class="imagenHome object-cover h-auto"
-        src="https://cdn.pixabay.com/photo/2022/06/21/14/30/background-7275953_1280.jpg" alt="Imagen de fondo">
-
-    <!-- Contenedor centrado -->
-    <div class="centerbody">
-        <h1>Únete a Inveyt</h1>
+<div style="width: 100%;height:90vh;background-image: url('https://cdn.pixabay.com/photo/2022/06/21/14/30/background-7275953_1280.jpg');display: flex;justify-content: center;align-items: center;background-repeat: round;">
+    <div style="background-color: rgb(255 248 248 / 22%); padding: 20px;border-radius: 15px;">
+        <center>
+            <h3>Login Inveyt</h3>
+        </center>
+        <style>
+            input{
+                margin: 10px;
+            }
+            input[type="text"],input[type="password"] {
+                border-radius: 5px;
+            }
+        </style>
+        
+        <form action="#" method="post">
+            <label for="usuario">Usuario: </label><input type="text" name="usuario" id=""><br>
+            <label for="contrasena">Contraseña: </label><input type="password" name="contrasena" id=""><br>
+            <input type="submit" value="Iniciar Sesion" style="background-color: black; border-radius: 5px;border-color: white; color: white;width: 100%;"><br>
+        </form>
+        <center>
+            <a href="#">Unete a Inveyt</a>
+        </center>
     </div>
 </div>
