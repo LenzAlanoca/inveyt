903a60fada82e720ad1d04b202c2de2fe8a5098d
LenzAlanoca <laalanoca3@umsa.bo>
2025-01-12 23:35:53 -0400

    ProfileController

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap');
+    </style>
+    <link rel="stylesheet" href="..\resources\css\app.css">
+    @vite('resources/css/app.css')
+    <title>Profile</title>
+</head>
+
+<body style="font-family: Comfortaa">
+    <div class="content">
+        <x-nav></x-nav>
+        <main>
+            <br><br><br><br><br><br><br><b><br><br><br>
+                <h1>Profile View</h1>
+                <br><br><br><br><br><br><br><b><br><br><br>
+
+        </main>
+
+        <x-footer></x-footer>
+    </div>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\SigninController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProductosController;
 use App\Http\Controllers\AcercadeController;
-
+use App\Http\Controllers\ProfileController;
+use Symfony\Component\HttpKernel\Profiler\Profile;
 
 Route::get('/', [InveytController::class, 'index']);
 
@@ -14,6 +15,7 @@ Route::get('/Singin', [SigninController::class, 'sigin']);
 Route::get('/Login', [LoginController::class, 'login']);
 Route::get('/Productos', [ProductosController::class, 'productos']);
 Route::get('/Acercade', [AcercadeController::class, 'acercade']);
+Route::get('/Profile', [ProfileController::class, 'profile']);
 
 Route::get('/perfil', function () {
     return "Bienvenido al perfil ";
